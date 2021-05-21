293561c0d60b0d4086626dd9b603802cf0e341d4
gsekhniashvili <gsekhniashvili@bog.ge>
2021-05-21 20:53:28 +0400

    xor you can learn with only one layer

---
+++ pytorch_implementations/simple_NNs/xor.py
@@ -0,0 +1,40 @@
+import torch
+
+
+class XOR_model(torch.nn.Module):
+    def __init__(self):
+        super(XOR_model, self).__init__()
+        self.fc_1 = torch.nn.Linear(in_features=2, out_features=2)
+        self.fc_2 = torch.nn.Linear(in_features=2, out_features=1)
+
+    def forward(self, x):
+        out = torch.sigmoid(self.fc_1(x))
+        out = torch.sigmoid(self.fc_2(out))
+        return out
+
+
+if __name__ == '__main__':
+    data = torch.tensor([[1, 1], [1, 0], [0, 1], [0, 0]], dtype=torch.float32, requires_grad=False)
+    targets = torch.tensor([0, 1, 1, 0], dtype=torch.float32, requires_grad=False).view((4, 1))
+
+    model = XOR_model()
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+    loss_fn = torch.nn.BCELoss()
+
+    epochs = 10000
+    model.train()
+    for i in range(epochs):
+        outputs = model(data)
+
+        loss = loss_fn(outputs, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # print(loss)
+        if i % 1000 == 0:
+            print(loss)
+
+    model.eval()
+    out = model(data)
+
